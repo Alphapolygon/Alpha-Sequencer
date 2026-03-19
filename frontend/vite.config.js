280fb6d72b99ea57322046bc464bd8536c512670
Alphapolygon <janne.piekkola@gmail.com>
2026-03-19 12:18:01 +0200

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,4 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
-  build: {
-    // Force standard script format instead of ES Modules
-    target: "es2015",
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    brotliSize: false,
-    modulePreload: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        // This is the magic key: It forces standard script execution without CORS issues!
-        format: 'iife' 
-      }
-    },
-  },
 })
